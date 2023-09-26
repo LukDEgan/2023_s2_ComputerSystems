5e69f04c83f02d7e0954d85ad4e4d1e8079e1983
Luke Egan <lukeegan2003@gmail.com>
2023-09-26 16:57:29 +0930

    overflow

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,6 +1,9 @@
 // Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+
+//R3 is min value so far
+//R4 is current element value
 // Put your code here.
 //check length is > 0
 @R2
@@ -32,17 +35,57 @@ D=D-M //current address - end address. If gt 0, current address > end address, t
 @END
 D;JGT
 @R1
+A=M
+D=M
+@R4
+M=D
+@R0
+D=M
+@R3
+M=D
+
+@R4
+M=!M
+D=D&M
+@R7
+M=D
+@R4
+M=!M
+D=M
+@R3
+M=!M
+D=D&M
+@R8
+M=D
+@R7
+D=D|M
+@SUB
+D;JGE
+@R1
+A=M
+D=M
+@SWAP
+D;JLT
+(SUB)
+
+
+
+@R1
 A=M //if not, load current element address into A
 D=M //load current element value into D
 @R0
 D=D-M //current value - curr min value >= 0. then  curr min <= curr value so move to next element
 @NEXT
 D;JGE
+
+(SWAP)
 @R1 // if not, swap elements
 A=M // load current element into A and get value into D
 D=M
 @R0 // load D into R0
 M=D
+
+
 (NEXT)
 @R1 //address moves up one to next element
 M=M+1
