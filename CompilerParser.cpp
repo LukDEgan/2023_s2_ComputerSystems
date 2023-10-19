a10567a81943c84f570649f1aae99ed04ca01c1c
Luke Egan <lukeegan2003@gmail.com>
2023-10-19 10:38:06 +1030

    WTF IS HAPPENING RN RAHHHHH

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -1,171 +1,168 @@
 #include "CompilerParser.h"
 
-
 /**
  * Constructor for the CompilerParser
  * @param tokens A linked list of tokens to be parsed
  */
-CompilerParser::CompilerParser(std::list<Token*> tokens) {
-}
+CompilerParser::CompilerParser(std::list<Token*> tokens) : tokens(tokens) {}
 
 /**
  * Generates a parse tree for a single program
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileProgram() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileProgram() { return NULL; }
 
 /**
  * Generates a parse tree for a single class
  * @return a ParseTree
  */
 ParseTree* CompilerParser::compileClass() {
-    return NULL;
+  ParseTree* classNode = new ParseTree("class", "");
+  mustBe("keyword", "class");
+  mustBe("identifier", current()->getValue());
+  mustBe("symbol", "{");
+  mustBe("symbol", "}");
+  return classNode;
 }
 
 /**
  * Generates a parse tree for a static variable declaration or field declaration
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileClassVarDec() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileClassVarDec() { return NULL; }
 
 /**
  * Generates a parse tree for a method, function, or constructor
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileSubroutine() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileSubroutine() { return NULL; }
 
 /**
  * Generates a parse tree for a subroutine's parameters
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileParameterList() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileParameterList() { return NULL; }
 
 /**
  * Generates a parse tree for a subroutine's body
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileSubroutineBody() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileSubroutineBody() { return NULL; }
 
 /**
  * Generates a parse tree for a subroutine variable declaration
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileVarDec() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileVarDec() { return NULL; }
 
 /**
  * Generates a parse tree for a series of statements
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileStatements() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileStatements() { return NULL; }
 
 /**
  * Generates a parse tree for a let statement
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileLet() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileLet() { return NULL; }
 
 /**
  * Generates a parse tree for an if statement
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileIf() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileIf() { return NULL; }
 
 /**
  * Generates a parse tree for a while statement
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileWhile() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileWhile() { return NULL; }
 
 /**
  * Generates a parse tree for a do statement
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileDo() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileDo() { return NULL; }
 
 /**
  * Generates a parse tree for a return statement
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileReturn() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileReturn() { return NULL; }
 
 /**
  * Generates a parse tree for an expression
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileExpression() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileExpression() { return NULL; }
 
 /**
  * Generates a parse tree for an expression term
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileTerm() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileTerm() { return NULL; }
 
 /**
  * Generates a parse tree for an expression list
  * @return a ParseTree
  */
-ParseTree* CompilerParser::compileExpressionList() {
-    return NULL;
-}
+ParseTree* CompilerParser::compileExpressionList() { return NULL; }
 
 /**
  * Advance to the next token
  */
-void CompilerParser::next(){
-    return;
+void CompilerParser::next() {
+  if (!tokens.empty()) {
+    tokens.pop_front();
+  }
+  return;
 }
 
 /**
  * Return the current token
  * @return the Token
  */
-Token* CompilerParser::current(){
-    return NULL;
+Token* CompilerParser::current() {
+  if (!tokens.empty()) {
+    return tokens.front();
+  }
+  return NULL;
 }
 
 /**
  * Check if the current token matches the expected type and value.
  * @return true if a match, false otherwise
  */
-bool CompilerParser::have(std::string expectedType, std::string expectedValue){
-    return false;
+bool CompilerParser::have(std::string expectedType, std::string expectedValue) {
+  if (!tokens.empty()) {
+    Token* currentToken = current();
+    if (currentToken->getValue() == expectedValue &&
+        currentToken->getType() == expectedType) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
  * Check if the current token matches the expected type and value.
- * If so, advance to the next token, returning the current token, otherwise throw a ParseException.
+ * If so, advance to the next token, returning the current token, otherwise
+ * throw a ParseException.
  * @return the current token before advancing
  */
-Token* CompilerParser::mustBe(std::string expectedType, std::string expectedValue){
-    return NULL;
+Token* CompilerParser::mustBe(std::string expectedType,
+                              std::string expectedValue) {
+  if (!tokens.empty()) {
+    Token* currentToken = current();
+    if (have(expectedType, expectedValue)) {
+      next();
+      return currentToken;
+    } else {
+      throw ParseException();
+    }
+  }
+  return NULL;
 }
 
 /**
@@ -173,5 +170,5 @@ Token* CompilerParser::mustBe(std::string expectedType, std::string expectedValu
  * You can use this ParseException with `throw ParseException();`
  */
 const char* ParseException::what() {
-    return "An Exception occurred while parsing!";
+  return "An Exception occurred while parsing!";
 }
